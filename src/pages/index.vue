4b4df8683f47be1f61c3fa84d13755bd1df3a746
xiaofan <ningfengzeng@gmail.com>
2025-01-17 18:13:33 +0800

    chore: reset media

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,10 +9,10 @@ function getFull(uri: string) {
     :img-src="getFull('../assets/IMG_20250115_162834.jpg')"
     :video-src="getFull('../assets/IMG_20250115_162834.mp4')"
   />
-  <LivePhoto
+  <!-- <LivePhoto
     :img-src="getFull('../assets/IMG_20250115_171107.jpg')"
     :video-src="getFull('../assets/IMG_20250115_171107.mp4')"
-  />
+  /> -->
   <LivePhoto
     :img-src="getFull('../assets/IMG_20250115_171418.jpg')"
     :video-src="getFull('../assets/IMG_20250115_171418.mp4')"
